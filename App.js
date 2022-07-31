04eafa92e0c6e41ba1b242a28f56084095623883
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-07-31 22:21:45 +0900

    [1.6] Creating The App

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button} from "react-native-web";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello! I made a RN App!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
