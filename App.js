693ea47608717df8243e84567aefa94ecbcce6a1
dev-eunjiLee <dev.eunji.lee@gmail.com>
2022-08-07 21:37:57 +0900

    [2.6] Styles part Two

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+console.log(SCREEN_WIDTH);
+
 export default function App() {
+  console.log("here");
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        contentContainerStyle={styles.weather}
+        showsHorizontalScrollIndicator={false}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27°</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27°</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27°</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27°</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -32,12 +54,10 @@ const styles = StyleSheet.create({
     fontSize: 68,
     fontWeight: "500",
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
     alignItems: "center",
+    width: SCREEN_WIDTH,
   },
   temp: { marginTop: 50, fontSize: 178 },
   description: { marginTop: -30, fontSize: 60 },
